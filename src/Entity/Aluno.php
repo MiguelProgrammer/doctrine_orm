bc0811d4fee71d15b11a4f52bb817c2cf253c726
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2022-01-18 23:30:12 -0300

    Criando a entidade Telefone

--- src/Entity/Aluno.php
+++ src/Entity/Aluno.php
@@ -2,6 +2,8 @@
 
 namespace Estudos\Doctrine\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Entity;
 
 /**
@@ -20,6 +22,16 @@ class Aluno
      */
     private $nome;
 
+    /**
+     *  @OneToMany(targetEntity="Telefone", mappedBy="Aluno")
+     */
+    private $telefones;
+
+    public function __construct()
+    {
+        $this->telefones = new ArrayCollection();
+    }
+
     public function getId(): int
     {
         return $this->id;
@@ -34,6 +46,18 @@ class Aluno
     {
         $this->nome = $nome;
         return $this;
+    }
+
+    public function addTelefone(Telefone $telefone)
+    {
+        $this->telefones->add($telefone);
+        $telefone->setAluno($this);
+        return $this;
+    }
+
+    public function getTelefones(): Collection
+    {
+        return $this->telefones;
     }
 
 
